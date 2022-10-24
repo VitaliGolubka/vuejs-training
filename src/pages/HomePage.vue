0e45d3f09c3f2adc05c905e829cdae84864b2e79
Golubka <jokkerit3@gmail.com>
2022-10-24 21:15:41 +0300

    temporary commit

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>Now you in home page</div>
+  <div>Now you in home page {{ a }}</div>
+  <div>
+    <div>
+    </div>
+  </div>
+  <button @click="handlePushClick">Push</button>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const a = ref(5);
+    onMounted(() => {
+      a.value = ++a.value;
+    });
+    const handlePushClick = () => {
+      a.value = ++a.value;
+    };
+    return {
+      a,
+      handlePushClick,
+    };
+  },
+});
+</script>
